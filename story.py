8a0f3d2acfab01c4023ef40534d87edf3f95b0d1
Tony Schembri <230397822+Bling12348@users.noreply.github.com>
2025-09-24 22:47:41 -0400

    Added a center path.

--- story.py
+++ story.py
@@ -7,6 +7,7 @@ def intro():
         right_path()
     else:
         print("You stand still, unsure what to do. The forest swallows you.")
+        print("")
 
 def left_path():
     print("You walk left and find a mysterious glowing sword stuck in a stone.")
